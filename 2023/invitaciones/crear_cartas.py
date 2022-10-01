4f1bd3f8b3b508c22ae251ccab7216389e286062
Gustavo Landfried <gustavolandfried@gmail.com>
2022-10-01 09:53:00 +0200

    Antes de enviar las invitaciones y antes de actualizar el creador de cartas para que no guarde ningun tex que no sea archivo fuente

--- 2023/invitaciones/crear_cartas.py
+++ 2023/invitaciones/crear_cartas.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import time
 import os
+from unidecode import unidecode
 
 data = pd.read_csv("perfiles/detalles_mas.csv", sep="|")
 
@@ -9,17 +10,19 @@ for index, persona in data[data["bayes"]>0.0].iterrows():
     #persona = data.iloc[0,:]
     #LT = ''.join(persona["Lugar de trabajo"].split(" & "))[0:-3]
     #print(LT)
+    nombre = " ".join(persona["Nombre"].split("\xa0")[1].split(" ")).title()
     apellido = "-".join(persona["Nombre"].split("\xa0")[0].split(" "))
+    apellido_texto = "-".join(persona["Nombre"].split("\xa0")[0].split(" "))
     folder = "cartas/{}".format(persona["Numero"])
     if not os.path.exists(folder):
         os.makedirs(folder)
-    archivo = "cartas/{}/{}-{}.tex".format(persona["Numero"],persona["Numero"],apellido)
+    archivo = "cartas/{}/{}-{}.tex".format(persona["Numero"],persona["Numero"],unidecode(apellido))
     os.system("cp cartas/carta_modelo.tex {}".format(archivo))
-    os.system("sed -i 's/APELLIDO-NOMBRE/{}/g' {}".format(persona["Nombre"], archivo))
+    os.system("sed -i 's/APELLIDO-NOMBRE/{} {}/g' {}".format(apellido_texto, nombre, archivo))
     os.system("sed -i 's/CARGO/{}/g' {}".format(persona["Categoría"], archivo))
     os.system("sed -i 's/..\/aux/..\/..\/aux/g' {}".format(archivo))
     os.system("cp cartas/makefile cartas/{}/makefile".format(persona["Numero"]))
-    os.system("sed -i 's/carta_modelo/{}-{}/g' cartas/{}/makefile".format(persona["Numero"],apellido, persona["Numero"]))
+    os.system("sed -i 's/carta_modelo/{}-{}/g' cartas/{}/makefile".format(persona["Numero"],unidecode(apellido), persona["Numero"]))
     os.system("make -C {}".format(folder))
-    os.system("cp cartas/{}/{}-{}.pdf cartas/pdf/{}-{}.pdf".format(persona["Numero"],persona["Numero"],apellido,persona["Numero"],apellido))
+    os.system("cp cartas/{}/{}-{}.pdf cartas/pdf/{}-{}.pdf".format(persona["Numero"],persona["Numero"],unidecode(apellido),persona["Numero"],unidecode(apellido)))
 
--- 2023/invitaciones/perfiles/envios.py
+++ 2023/invitaciones/perfiles/envios.py
@@ -3,7 +3,6 @@ import requests
 import csv
 import pandas as pd
 from collections import defaultdict
-import pandas as pd
 import re
 import time
 import os
@@ -13,43 +12,51 @@ from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 import time
 from selenium.webdriver.common.by import By
 import selenium
+from unidecode import unidecode
 
 data = pd.read_csv("detalles_mas.csv", sep="|")
 
+i = 0
+for index, persona in data[data["bayes"]>0.0].iterrows():#index=578
+    i +=1
+    numero = data["Numero"][index]
+    nombre = data["Nombre"][index]
+    apellido = "-".join(nombre.split("\xa0")[0].split(" "))
+    nombre = " ".join(nombre.split("\xa0")[1].split(" ")).title()
 
-numero = data["Numero"][578]
-nombre = data["Nombre"][578]
-apellido = "-".join(nombre.split("\xa0")[0].split(" "))
+    print(index, apellido, numero)
+    url = 'https://www.conicet.gov.ar/new_scp/mail.php?id={}&detalle=yes&esprimera=1'.format(numero)
 
-url = 'https://www.conicet.gov.ar/new_scp/mail.php?id={}&detalle=yes&esprimera=1'.format(numero)
+    url_carta = 'http://bayesdelsur.com.ar/invitaciones/{}-{}.pdf'.format(numero, unidecode(apellido))
+    page_carta = requests.get(url_carta,timeout=5)
+    assert (page_carta.status_code == 200)
 
-url_carta = 'http://bayesdelsur.com.ar/invitaciones/{}-{}.pdf'.format(numero, apellido)
-page_carta = requests.get(url_carta,timeout=5)
-assert (page_carta.status_code == 200)
+    driver = webdriver.Firefox()
+    driver.get(url)
+    driver.find_elements(By.XPATH, '//td/img/')
+    imagen = driver.find_element(By.XPATH, "//td[@class='general_texto']/img").get_attribute("src")
+    clave = imagen.split("k=")[1]
 
-driver = webdriver.Firefox()
-driver.get(url)
-driver.find_elements(By.XPATH, '//td/img/')
-imagen = driver.find_element(By.XPATH, "//td[@class='general_texto']/img").get_attribute("src")
-clave = imagen.split("k=")[1]
+    input_0=driver.find_element(by=By.NAME, value= 'remitente')
+    input_0.clear()
+    input_0.send_keys('Comisión Organizadora del Congreso Bayesiano Plurinacional')
+    input_1=driver.find_element(by=By.NAME, value= 'de_mail')
+    input_1.clear()
+    input_1.send_keys('bayesdelsur@gmail.com')
+    input_2=driver.find_element(by=By.NAME, value= 'subject')
+    input_2.clear()
+    input_2.send_keys('Invitación especial')
+    input_3=driver.find_element(by=By.NAME, value= 'message')
+    input_3.clear()
+    input_3.send_keys('{},\n\nUn grupo de investigadores de diferentes regiones del país, reunidos en torno a la aplicación del enfoque bayesiano de la teoría probabilidad, nos dirigimos a usted para invitarle personalmente a participar, activa o pasivamente, de la organización del primer Congreso Bayesiano Plurinacional, a realizarse los días 4 y 5 de Agosto del año 2023 en las instalaciones del NODO tecnológico de Santiago del Estero, Argentina.\n\nSi bien la aplicación estricta de la teoría de la probabilidad (enfoque bayesiano) ha mostrado ser la lógica ideal en contextos de incertidumbre, su adopción se vio históricamente limitada debido al alto costo computacional que impone la evaluación de todo el espacio de hipótesis. Aunque en las últimas décadas estas limitaciones han sido superadas en gran medida gracias al crecimiento en la capacidad de cmputo y el desarrollo de métodos eficientes de aproximación, la inercia histórica parece ser actualmente su limitación principal.\n\nEl Congreso tiene por objetivo hacer crecer las comunidades bayesianas en Argentina y la Región, reuniendo a estudiantes, docentes, investigadores, emprendedores, practicantes que utilizan, desarrollan o implementan métodos bayesianos en sus respectivos trabajos. Entre los tópicos a tratar se incluye: modelado probabilístico, razonamiento causal, toma de decisiones, programación probabilística, teoría de la información, inteligencia artificial, ciencia de datos, epistemología y metodología, estadística aplicada, pedagogía de la probabilidad, entre otros.\n\nEl apoyo a la organización del Congreso pueden ser: simbólico, ofreciendo su nombre como aval; activo, participando de alguna tarea; o en especies, aportando bienes o dinero. Puede indicar el tipo de participación completando el siguiente formulario: https://bit.ly/apoyo_cbp . La primera circular informativa se encuentra disponible en el sitio bayesdelsur.com.ar\n\nPuede encontrar su invitación personal en el siguiente link: {}\n\nSin otro particular, le saluda a usted muy atentamente la Comisión organizadora del Congreso Bayesiano Plurinacional. \n\n Dr MARTIN, Osvaldo. Dr. DÍAZ, Rodrigo. Lic. SERRANO, María Gimena. Lic. LANDFRIED, Gustavo.'.format(nombre,url_carta))
+    input_4=driver.find_element(by=By.NAME, value= 'tmptxt')
+    input_4.clear()
+    input_4.send_keys(clave)
 
-input_0=driver.find_element(by=By.NAME, value= 'remitente')
-input_0.clear()
-input_0.send_keys('Comisión Organizadora del Congreso Bayesiano Plurinacional')
-input_1=driver.find_element(by=By.NAME, value= 'de_mail')
-input_1.clear()
-input_1.send_keys('bayesdelsur@gmail.com')
-input_2=driver.find_element(by=By.NAME, value= 'subject')
-input_2.clear()
-input_2.send_keys('Invitación especial')
-input_3=driver.find_element(by=By.NAME, value= 'message')
-input_3.clear()
-input_3.send_keys('{},\n\nUn grupo de investigadores de todo el país, reunidos en torno a la aplicación del enfoque bayesiano de la teoría probabilidad, nos dirigimos a usted para invitarle personalmente a participar, activa o pasivamente, de la organización del Primer Congreso Bayesiano Plurinacional. \n \nEl Congreso tiene por objetivo hacer crecer las comunidades Bayesianas en Argentina y la Región, reuniendo a estudiantes, docentes, investigadores, emprendedores, practicantes que utilizan, desarrollan o implementan métodos Bayesianos en sus respectivos trabajos. \n\nLa participación pueden ser pasiva, a través de un aval de apoyo (completando el siguiente formulario https://forms.gle/Bhoph97NpNPdA7LTA, o activa a través del contacto directo con la organización, enviando un correo a bayesdelsur@gmail.com. \n\nPuede encontrar su invitación personal en el siguiente link: {}\n\n Sin otro particular, le saluda a usted muy atentamente la comisión organizadora del Congreso Bayesiano Plurinacional. \n\n- Dr. MARTIN, Osvaldo. Investigador Conicet. IMASL-CONICET (San Luis) \n- Dr. Rodrigo Díaz. Investigador Conicet. Universidad Nacional de San Martín. \n- Lic. María Gimena Serrano. Secretaría de Ciencia y Técnica. Gobierno de Santiago del Estero \n- Lic. Gustavo Landfried. Doctorando en Cs de la Computación. Universidad de Buenos Aires'.format(nombre,url_carta))
-input_4=driver.find_element(by=By.NAME, value= 'tmptxt')
-input_4.clear()
-input_4.send_keys(clave)
+    from selenium.webdriver.common.action_chains import ActionChains
+    enviar=driver.find_element(by=By.CLASS_NAME, value= 'enviar_mail_imagen')
+    ActionChains(driver).move_to_element(enviar).click(enviar).perform()
+    enviar.click()
 
-from selenium.webdriver.common.action_chains import ActionChains
-enviar=driver.find_element(by=By.CLASS_NAME, value= 'enviar_mail_imagen')
-ActionChains(driver).move_to_element(enviar).click(enviar).perform()
-enviar.click()
+    print(i, input_4)
+
