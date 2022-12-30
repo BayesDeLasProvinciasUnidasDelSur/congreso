2889f9c14fca205e50d50d83ee8a9df32c66ad6a
Gustavo Landfried <gustavolandfried@gmail.com>
2022-12-30 12:32:10 +0100

    Descarga finalizada. Análisis preliminar de datos

--- 2023/invitaciones/perfiles/scopus/autores_analisis.py
+++ 2023/invitaciones/perfiles/scopus/autores_analisis.py
@@ -1,13 +1,65 @@
 import pickle
+import networkx as nx
 import csv
 import pandas as pd
 from collections import defaultdict
 import re
 import time
 import os
+import matplotlib.pyplot as plt
+import itertools
+
+# DOI metadata
+# https://search.crossref.org/
 
 with open('autores_datos.pickle', 'rb') as handle:
     datos = pickle.load(handle)
+
+with open('papers_datos.pickle', 'rb') as handle:
+    papers = pickle.load(handle)
+
+k_ejemplo_paper = ('56389488100', 'Organization, evolution and transcriptional profile of hexamerin genes of the parasitic wasp Nasonia vitripennis (Hymenoptera: Pteromalidae)')
+
+def nodos_ejes(pais):
+    nodos = [ k for k in datos if pais in datos[k]["paises"]]
+    ejes= [ [c for c in itertools.combinations(
+set(papers[k]["autores"]).intersection(nodos), 2) ] for k in papers]
+    ejes= [e for xs in ejes for e in xs]
+    return nodos, ejes
+
+
+
+paises = ['Argentina', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia, Plurinational State of', 'Peru', 'Colombia', 'Ecuador', 'Venezuela, Bolivarian Republic of', 'Cuba',  'Mexico', 'Brazil', 'Nicaragua', 'Honduras', 'Guatemala']
+autores_por_pais = {  p: sum([1 for k in datos if p in datos[k]["paises"] ])  for p in paises }
+autores_por_pais
+
+sorted(autores_por_pais)
+
+NODOS_EJES = { p: nodos_ejes(p) for p in paises  }
+
+Gs = []
+for p in paises:
+    plt.close()
+    Gs.append(nx.Graph())
+    Gs[-1].add_nodes_from(NODOS_EJES[p][0])
+    Gs[-1].add_edges_from(NODOS_EJES[p][1])
+    nx.draw(Gs[-1], node_size = 10)
+    plt.savefig("figuras/{}.pdf".format(p),bbox_inches='tight')
+    plt.close()
+
+
+
+
+{ p:[c.number_of_nodes()/G.number_of_nodes() for c in sorted(nx.connected_components(G), key=len, reverse=True)][0:2] for G,p in zip(Gs, paises) if len(G) > 0 }
+
+
+
+set([q for k in papers for q in papers[k]])
+set([q for k in datos for q in datos [k]])
+
+
+[k for k in papers]
+
 
 datos.keys()
 
@@ -27,3 +79,19 @@ for k in datos:
 i
 j
 af
+
+sum([len(datos[k]["papers"])>1 for k in datos])
+
+
+
+autores_con_contacto_por_pais = {  p: sum([1 for k in datos if (p in datos[k]["paises"]) and len(datos[k]["mails"])>0 ])  for p in paises }
+autores_con_contacto_por_pais
+
+
+[datos[k]["nombre"] for k in datos if ("Argentina" in datos[k]["paises"]) and len(datos[k]["mails"])>0 ]
+[(k,datos[k]["nombre"]) for k in datos if ("Argentina" in datos[k]["paises"]) and len(datos[k]["mails"])>0 ]
+
+for k in datos:
+    if ("Argentina" in datos[k]["paises"]) and len(datos[k]["mails"])>0 :
+        print(datos[k])
+        print("")
--- 2023/invitaciones/perfiles/scopus/scopus.py
+++ 2023/invitaciones/perfiles/scopus/scopus.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 import re
 import time
 import os
+from selenium.common.exceptions import MoveTargetOutOfBoundsException
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
@@ -27,7 +28,7 @@ from unidecode import unidecode
 from pygame import mixer
 mixer.init()
 sound=mixer.Sound("bell.wav")
-
+sound2=mixer.Sound("bell2.wav")
 
 def window_bounds(driver):
     win_upper_bound = driver.execute_script('return window.pageYOffset')
@@ -56,9 +57,11 @@ def element_completely_viewable(driver, elem, win_left_bound, win_right_bound, w
 def autores(info_personas, xpath_personas, driver_paper):
     affil_links = []
     autor_links = []
+    autor_posicion = []
     for i in range(len(info_personas)):#i=43
         # iks
         print(i)
+        autor_posicion.append(i)
         print(1)
         ActionChains(driver_paper).move_to_element(info_personas[i]).click(info_personas[i]).perform()#info_personas[i].click()
         #
@@ -75,7 +78,7 @@ def autores(info_personas, xpath_personas, driver_paper):
         x_elemento = driver_paper.find_element(by=By.XPATH, value= '//article/div[2]/section/div[2]/div[1]/div[2]//header/div[1]/els-button[1]')
         ActionChains(driver_paper).move_to_element(x_elemento).click(x_elemento).perform()
     print(5)
-    return affil_links, autor_links
+    return affil_links, autor_links, autor_posicion
 
 
 # Set up the Firefox profile with the desired proxy settings
@@ -84,35 +87,46 @@ profile.set_preference("network.proxy.type", 1)
 profile.set_preference("network.proxy.socks", "localhost")
 profile.set_preference("network.proxy.socks_port", 2000)
 
-driver = webdriver.Firefox(firefox_profile=profile)
+#driver = webdriver.Firefox(firefox_profile=profile)
 driver_paper = webdriver.Firefox(firefox_profile=profile)
 
 url= "https://www.scopus.com/results/results.uri?sort=plf-f&src=s&st1=%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29&st2=argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador&sid=a83350921ff83885ce535de899e04f09&sot=b&sdt=b&sl=239&s=%28TITLE-ABS-KEY%28%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29%29+AND+AFFILCOUNTRY%28argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador%29%29&origin=searchbasic&editSaveSearch=&yearFrom=Before+1960&yearTo=2022"
 
-url = "https://www.scopus.com/results/results.uri?sort=plf-f&src=s&st1=%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29&st2=argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador&sid=107c6cee5bce6d66a1a9e09145f25c0b&sot=b&sdt=b&sl=239&s=%28TITLE-ABS-KEY%28%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29%29+AND+AFFILCOUNTRY%28argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador%29%29&origin=searchbasic&editSaveSearch=&yearFrom=Before+1960&yearTo=2017"
+url = "https://www.scopus.com/results/results.uri?sort=plf-f&src=s&st1=%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29&st2=argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador&sid=107c6cee5bce6d66a1a9e09145f25c0b&sot=b&sdt=b&sl=239&s=%28TITLE-ABS-KEY%28%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29%29+AND+AFFILCOUNTRY%28argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador%29%29&origin=searchbasic&editSaveSearch=&yearFrom=Before+1960&yearTo=2010"
 
-driver.get(url)
-tree = html.fromstring(driver.page_source)
+#driver.get(url)
+#tree = html.fromstring(driver.page_source)
 
-nombre_papers = tree.xpath('//a[@class="ddmDocTitle"]/text()')
-link_papers = tree.xpath('//a[@class="ddmDocTitle"]/@href')
+#nombre_papers = tree.xpath('//a[@class="ddmDocTitle"]/text()')
+#link_papers = tree.xpath('//a[@class="ddmDocTitle"]/@href')
 
-len(nombre_papers)
-len(link_papers )
+#len(nombre_papers)
+#len(link_papers )
 
-total = int(tree.xpath('//span[@class="resultsCount"]')[0].text.replace('\n','').replace(',',''))
+#total = int(tree.xpath('//span[@class="resultsCount"]')[0].text.replace('\n','').replace(',',''))
 
 
-driver_paper.get(link_papers[0])
+driver_paper.get("https://www.scopus.com/record/display.uri?eid=2-s2.0-77950358311&origin=resultslist&sort=plf-f&src=s&st1=%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29&st2=argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador&nlo=&nlr=&nls=&sid=107c6cee5bce6d66a1a9e09145f25c0b&sot=b&sdt=b&sl=258&s=%28TITLE-ABS-KEY%28%28bayes+AND+NOT+%28%22naive+bayes%22%29%29+OR+%28%22causal+inference%22+AND+NOT+%28%22naive+bayes%22%29%29%29+AND+AFFILCOUNTRY%28argentina+OR+chile+OR+uruguay+OR+paraguay+OR+bolivia+OR+per%c3%ba+OR+colombia+OR+venezuela+OR+brazil+OR+m%c3%a9xico+OR+cuba+OR+ecuador%29%29+AND+PUBYEAR+%3c+2011&relpos=106&citeCnt=38&searchTerm=")
 
-n_paper = 817
+
+
+n_paper = 890
 autores_datos = dict()
+papers_datos = dict()
+
 with open('autores_datos.pickle', 'rb') as handle:
     autores_datos = pickle.load(handle)
 
+with open('papers_datos.pickle', 'rb') as handle:
+    papers_datos = pickle.load(handle)
 
-t=0
-while t < 4:
+len(papers_datos)
+len(autores_datos)
+
+total = 2000
+
+t=0; r=0
+while t < 5:
     try:
         while n_paper < total:
             # Levanto la página del paper.
@@ -120,13 +134,13 @@ while t < 4:
             next_link_xpath = [e.getroottree().getpath(e) for e in tree_paper.xpath('//section/nav/ul/li[@class="nextLink"]') ][0]
             record_page_count_xpath = [e.getroottree().getpath(e) for e in tree_paper.xpath('//section/nav/ul/li[@class="recordPageCount"]') ][0]
             #
-            next_paper = driver_paper.find_element(By.XPATH, next_link_xpath)
             current_number_of_paper = driver_paper.find_element(By.XPATH, record_page_count_xpath).text.split("of")
             n_paper = int(current_number_of_paper[0].replace(',',''))
             print("paper: ", n_paper )
             #
-            info_personas = driver_paper.find_elements(By.XPATH, '//section/div[2]/div[1]/ul/li/els-button')
-            xpath_personas = [e.getroottree().getpath(e) for e in tree_paper.xpath('//section/div[2]/div[1]/ul/li/els-button') ]
+            personas_path = [e.getroottree().getpath(e) for e in tree_paper.xpath('//div[@data-testid="author-list"]')][0]
+            info_personas = driver_paper.find_elements(By.XPATH, personas_path+'/div[1]/ul/li/els-button')
+            xpath_personas = [e.getroottree().getpath(e) for e in tree_paper.xpath(personas_path+'/div[1]/ul/li/els-button') ]
             #
             nombre_personas = [e.text for e in info_personas ]
             #
@@ -140,12 +154,19 @@ while t < 4:
                 affil_links = []
                 affil_texts = []
                 autor_links = []
+                autor_posicion = []
                 for i in range(len(info_personas)):#i=0
                     # iks
                     print("Persona: ", i, ". ", end="")
+                    autor_posicion.append(i)
                     print(1, end="")
-                    ActionChains(driver_paper).move_to_element(info_personas[i]).click(info_personas[i]).perform()#info_personas[i].click()
-                    #
+                    try:
+                        ActionChains(driver_paper).move_to_element(info_personas[i]).click(info_personas[i]).perform()#info_personas[i].click()
+                    except MoveTargetOutOfBoundsException:
+                        driver_paper.execute_script("arguments[0].scrollIntoView();", info_personas[i])
+                        time.sleep(0.5)
+                        info_personas = driver_paper.find_elements(By.XPATH, personas_path+'/div[1]/ul/li/els-button')
+                        ActionChains(driver_paper).move_to_element(info_personas[i]).click(info_personas[i]).perform()
                     print(2, end="")
                     affil_element = driver_paper.find_elements(By.XPATH, xpath_personas[i].split("/els-button")[0]+"/div/div/div/div/div/els-stack/els-stack[1]/els-stack//a" )
                     list_affil = [e.get_attribute('href') for e in affil_element ]
@@ -160,11 +181,36 @@ while t < 4:
                     #x_elemento = driver_paper.find_element(by=By.XPATH, value= '//header/div[1]/els-button[1]')
                     #ActionChains(driver_paper).move_to_element(x_elemento).click(x_elemento).perform()
                     ActionChains(driver_paper).move_to_element(info_personas[i]).click(info_personas[i]).perform()#info_personas[i].click()
-                journal_link = driver_paper.find_element(By.XPATH, "//article/div/div/div/a").get_attribute("href")
-                journal_name = driver_paper.find_element(By.XPATH, "//article/div/div/div/a").text
+                try:
+                    journal_link = driver_paper.find_element(By.XPATH, "//article/div/div/div/a").get_attribute("href")
+                except:
+                    journal_link = ""
+                try:
+                    journal_name = driver_paper.find_element(By.XPATH, "//article/div/div/div/a").text
+                except:
+                    journal_name = ""
+                try:
+                    publication_date = int(driver_paper.find_element(By.XPATH, "//article/div/div/div").text.split(" ")[-1])
+                except:
+                    publication_date = 0
                 paper_name = driver_paper.find_element(By.XPATH, "//section/div/div/h2").text
-                #
-                #journal_link
+                doc_type = [e.text.split("\n")[1] for e in driver_paper.find_elements(By.XPATH, "//article/div[2]/aside/div[1]/div[1]/div[1]/dl") if "Document type\n" in e.text]
+                source_type = [e.text.split("\n")[1] for e in driver_paper.find_elements(By.XPATH, "//article/div[2]/aside/div[1]/div[1]/div[1]/dl") if "Source type\n" in e.text]
+                doi = [e.text.split("\n")[1] for e in driver_paper.find_elements(By.XPATH, "//article/div[2]/aside/div[1]/div[1]/div[1]/dl") if "DOI\n" in e.text]
+                issn = [e.text.split("\n")[1] for e in driver_paper.find_elements(By.XPATH, "//article/div[2]/aside/div[1]/div[1]/div[1]/dl") if "ISSN\n" in e.text]
+                try:
+                    abstract_path = [e.getroottree().getpath(e) for e in tree_paper.xpath('//h3[@id="abstract"]')][0]
+                    abstract = driver_paper.find_element(By.XPATH, abstract_path.split("h3")[0]+'div').text
+                except:
+                    abstract = "empty"
+                try:
+                    key_words = driver_paper.find_element(By.XPATH, "//article/div[3]/section[1]/div[1]/div[2]").text.split(";")
+                except:
+                    key_words = []
+                autor_id = [a.split("authorId=")[1].split("&")[0] for a in autor_links]
+                key_paper = (autor_id[0], paper_name)
+                if not (key_paper in papers_datos):
+                    papers_datos[key_paper] = {"titulo": paper_name, "abstract": abstract, "palabras": key_words, "ISSN": issn, "DOI": doi, "autores": autor_id, "journal": journal_name, "source_type": source_type, "document_type": doc_type, "publication_date": publication_date  }
                 #
                 n_personas = len(nombre_personas)
                 #
@@ -183,35 +229,50 @@ while t < 4:
                             paises[-1].append(pycountry.countries.search_fuzzy(a.split(",")[-1])[0].name)
                         except:
                             paises[-1].append("desconocido")
-                    latino.append([c for c in paises[-1] if c in ['Argentina', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia', 'Peru', 'Colombia', 'Ecuador', 'Venezuela', 'Cuba',  'Mexico', 'Brazil'] ])
-                autor_id = [a.split("authorId=")[1].split("&")[0] for a in autor_links]
+                    latino.append([c for c in paises[-1] if c in ['Argentina', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia, Plurinational State of', 'Peru', 'Colombia', 'Ecuador', 'Venezuela, Bolivarian Republic of', 'Cuba',  'Mexico', 'Brazil', 'Nicaragua', 'Honduras', 'Guatemala'] ])
                 for a in range(n_personas):
                     if not (autor_id[a] in autores_datos):
-                        autores_datos[autor_id[a]] = {"nombre": nombre_personas[a], "paises": set(paises[a]), "latinos": set(latino[a]), "mails": set() if mails[a] is None else set([mails[a]]), "affil": set(affil_texts[a]) }
+                        autores_datos[autor_id[a]] = {"nombre": nombre_personas[a], "paises": set(paises[a]), "latinos": set(latino[a]), "mails": set() if mails[a] is None else set([mails[a]]), "affil": set(affil_texts[a]), "papers": set([(autor_posicion[a], paper_name)]) }
                     else:
-                        autores_datos[autor_id[a]]["paises"].union(set(paises[a]))
-                        autores_datos[autor_id[a]]["latinos"].union(set(latino[a]))
-                        autores_datos[autor_id[a]]["affil"].union(set(affil_texts[a]))
+                        autores_datos[autor_id[a]]["paises"].update(set(paises[a]))
+                        autores_datos[autor_id[a]]["latinos"].update(set(latino[a]))
+                        autores_datos[autor_id[a]]["affil"].update(set(affil_texts[a]))
+                        autores_datos[autor_id[a]]["papers"].update(set([(autor_posicion[a], paper_name)]))
                         if not (mails[a] is None):
-                            autores_datos[autor_id[a]]["mails"].union(set([mails[a]]))
+                            autores_datos[autor_id[a]]["mails"].update(set([mails[a]]))
                 if n_paper%10 == 0:
                     with open('autores_datos.pickle', 'wb') as handle:
                         pickle.dump(autores_datos, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                ActionChains(driver_paper).move_to_element(next_paper).click(next_paper).perform()#info_personas[i].click()
+                    with open('papers_datos.pickle', 'wb') as handle:
+                        pickle.dump(papers_datos, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                next_paper = driver_paper.find_element(By.XPATH, next_link_xpath)
+                try:
+                    ActionChains(driver_paper).move_to_element(next_paper).click(next_paper).perform()
+                except MoveTargetOutOfBoundsException:
+                    driver_paper.execute_script("arguments[0].scrollIntoView();", next_paper)
+                    time.sleep(0.5)
+                    next_paper = driver_paper.find_element(By.XPATH, next_link_xpath)
+                    ActionChains(driver_paper).move_to_element(next_paper).click(next_paper).perform()
                 print(driver_paper.current_url)
                 time.sleep(6)
                 t=0
+                r=0
             except:
                 print("except")
                 time.sleep(2)
+                r+=1
+                if r > 5:
+                    sound.play()
     except:
         t+=1
+        time.sleep(1)
 
+sound.play()
+sound.play()
 sound.play()
 
 
 
 
-
-
+driver_paper.close()
 
